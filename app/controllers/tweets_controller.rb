36df3286458d40dbc1be43c69b9690629f53dfa8
yukimi.y <yyy.yukimi@gmail.com>
2021-04-01 07:51:25 +0900

    ツイート作成，投稿のアクションとビューを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,24 @@ class TweetsController < ApplicationController
   def index
     
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+      flash[:notice] = "ツイートを投稿しました"
+    else
+      redirect_to new_tweet_path
+      flash[:alert] = "ツイートに失敗しました"
+    end
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+    end
 end
